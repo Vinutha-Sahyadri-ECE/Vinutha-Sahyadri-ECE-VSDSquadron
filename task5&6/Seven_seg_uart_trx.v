32edab4d83a0482b6e05cc15171eddb1e5135175
Vinutha-Sahyadri-ECE <vinutha.ec22@sahyadri.edu.in>
2025-05-18 11:08:23 +0530

    Update Seven_seg_uart_trx.v

--- task5&6/Seven_seg_uart_trx.v
+++ task5&6/Seven_seg_uart_trx.v
@@ -1,1 +1,47 @@
+## 🧩 `uart_trx.v`
 
+**UART Receiver module in 8N1 format at 9600 baud**
+
+```verilog
+module uart_rx_8n1 (
+    input clk,
+    input rx,
+    output reg [7:0] rxbyte = 0,
+    output reg received = 0
+);
+
+    reg [3:0] bitindex = 0;
+    reg [7:0] data = 0;
+    reg [12:0] clkcount = 0;
+    reg busy = 0;
+    reg rx_sync = 1;
+
+    parameter BAUD_TICKS = 5208;  // 50 MHz / 9600 baud
+
+    always @(posedge clk) begin
+        rx_sync <= rx;
+
+        if (!busy) begin
+            received <= 0;
+            if (rx_sync == 0) begin
+                busy <= 1;
+                clkcount <= BAUD_TICKS / 2;
+                bitindex <= 0;
+            end
+        end else begin
+            if (clkcount == 0) begin
+                clkcount <= BAUD_TICKS;
+                if (bitindex < 8) begin
+                    data[bitindex] <= rx_sync;
+                    bitindex <= bitindex + 1;
+                end else if (bitindex == 8) begin
+                    rxbyte <= data;
+                    received <= 1;
+                    busy <= 0;
+                end
+            end else begin
+                clkcount <= clkcount - 1;
+            end
+        end
+    end
+endmodule
